d323cf2824c3f119911c62e810ec5d420bcc1236
fengwenbo <fwb02215164@antgroup.com>
2025-08-13 16:27:51 +0800

    Add double and long type test

--- test/hotspot/jtreg/compiler/jeandle/relocation/TestRelocation.java
+++ test/hotspot/jtreg/compiler/jeandle/relocation/TestRelocation.java
@@ -33,61 +33,193 @@ import jdk.test.lib.Asserts;
 
 public class TestRelocation {
     public static void main(String[] args) {
-        Asserts.assertEquals(20.0f, test1(1.0f));
-        Asserts.assertEquals(24.0f, test2(1.0f));
-        Asserts.assertEquals(29.0f, test3(1.0f));
-        Asserts.assertEquals(32.0f, test4(1.0f));
-        Asserts.assertEquals(37.0f, test5(1.0f));
-        Asserts.assertEquals(47.0f, test6(1.0f));
-    }
-    private static float callee1(float a) {
-      return a += 2.0f;
+        Asserts.assertEquals(20.0f, testFloat1(1.0f));
+        Asserts.assertEquals(24.0f, testFloat2(1.0f));
+        Asserts.assertEquals(29.0f, testFloat3(1.0f));
+        Asserts.assertEquals(32.0f, testFloat4(1.0f));
+        Asserts.assertEquals(37.0f, testFloat5(1.0f));
+        Asserts.assertEquals(47.0f, testFloat6(1.0f));
+
+        Asserts.assertEquals(20.0, testDouble1(1.0));
+        Asserts.assertEquals(24.0, testDouble2(1.0));
+        Asserts.assertEquals(29.0, testDouble3(1.0));
+        Asserts.assertEquals(32.0, testDouble4(1.0));
+        Asserts.assertEquals(37.0, testDouble5(1.0));
+        Asserts.assertEquals(47.0, testDouble6(1.0));
+
+        Asserts.assertEquals(20L, testLong1(1L));
+        Asserts.assertEquals(24L, testLong2(1L));
+        Asserts.assertEquals(29L, testLong3(1L));
+        Asserts.assertEquals(32L, testLong4(1L));
+        Asserts.assertEquals(37L, testLong5(1L));
+        Asserts.assertEquals(47L, testLong6(1L));
     }
 
-    private static float callee2(float a) {
-      return a += 3.0f;
+    private static float calleeFloat1(float a) {
+        return a += 2.0f;
     }
 
-    private static float test1(float n) {
-        n += callee1(n);
-        n += callee2(n);
+    private static float calleeFloat2(float a) {
+        return a += 3.0f;
+    }
+
+    private static double calleeDouble1(double a) {
+        return a += 2.0;
+    }
+
+    private static double calleeDouble2(double a) {
+        return a += 3.0;
+    }
+
+    private static long calleeLong1(long a) {
+        return a += 2;
+    }
+
+    private static long calleeLong2(long a) {
+        return a += 3;
+    }
+
+    private static float testFloat1(float n) {
+        n += calleeFloat1(n);
+        n += calleeFloat2(n);
         n += 4.0f;
         n += 5.0f;
         return n;
     }
-    private static float test2(float n) {
-        n += callee1(n);
+
+    private static float testFloat2(float n) {
+        n += calleeFloat1(n);
         n += 4.0f;
-        n += callee2(n);
+        n += calleeFloat2(n);
         n += 5.0f;
         return n;
     }
-    private static float test3(float n) {
-        n += callee1(n);
+
+    private static float testFloat3(float n) {
+        n += calleeFloat1(n);
         n += 4.0f;
         n += 5.0f;
-        n += callee2(n);
+        n += calleeFloat2(n);
         return n;
     }
-    private static float test4(float n) {
+
+    private static float testFloat4(float n) {
         n += 4.0f;
-        n += callee1(n);
-        n += callee2(n);
+        n += calleeFloat1(n);
+        n += calleeFloat2(n);
         n += 5.0f;
         return n;
     }
-    private static float test5(float n) {
+
+    private static float testFloat5(float n) {
         n += 4.0f;
-        n += callee1(n);
+        n += calleeFloat1(n);
         n += 5.0f;
-        n += callee2(n);
+        n += calleeFloat2(n);
         return n;
     }
-    private static float test6(float n) {
+
+    private static float testFloat6(float n) {
         n += 4.0f;
         n += 5.0f;
-        n += callee1(n);
-        n += callee2(n);
+        n += calleeFloat1(n);
+        n += calleeFloat2(n);
+        return n;
+    }
+
+    private static double testDouble1(double n) {
+        n += calleeDouble1(n);
+        n += calleeDouble2(n);
+        n += 4.0;
+        n += 5.0;
+        return n;
+    }
+
+    private static double testDouble2(double n) {
+        n += calleeDouble1(n);
+        n += 4.0;
+        n += calleeDouble2(n);
+        n += 5.0;
+        return n;
+    }
+
+    private static double testDouble3(double n) {
+        n += calleeDouble1(n);
+        n += 4.0;
+        n += 5.0;
+        n += calleeDouble2(n);
+        return n;
+    }
+
+    private static double testDouble4(double n) {
+        n += 4.0;
+        n += calleeDouble1(n);
+        n += calleeDouble2(n);
+        n += 5.0;
+        return n;
+    }
+
+    private static double testDouble5(double n) {
+        n += 4.0;
+        n += calleeDouble1(n);
+        n += 5.0;
+        n += calleeDouble2(n);
+        return n;
+    }
+
+    private static double testDouble6(double n) {
+        n += 4.0;
+        n += 5.0;
+        n += calleeDouble1(n);
+        n += calleeDouble2(n);
+        return n;
+    }
+
+    private static long testLong1(long n) {
+        n += calleeLong1(n);
+        n += calleeLong2(n);
+        n += 4;
+        n += 5;
+        return n;
+    }
+
+    private static long testLong2(long n) {
+        n += calleeLong1(n);
+        n += 4;
+        n += calleeLong2(n);
+        n += 5;
+        return n;
+    }
+
+    private static long testLong3(long n) {
+        n += calleeLong1(n);
+        n += 4;
+        n += 5;
+        n += calleeLong2(n);
+        return n;
+    }
+
+    private static long testLong4(long n) {
+        n += 4;
+        n += calleeLong1(n);
+        n += calleeLong2(n);
+        n += 5;
+        return n;
+    }
+
+    private static long testLong5(long n) {
+        n += 4;
+        n += calleeLong1(n);
+        n += 5;
+        n += calleeLong2(n);
+        return n;
+    }
+    
+    private static long testLong6(long n) {
+        n += 4;
+        n += 5;
+        n += calleeLong1(n);
+        n += calleeLong2(n);
         return n;
     }
 }
