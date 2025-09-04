e27d60340afe7f085b175e101d9b31e3477dd887
hexuejin <hexuejin.hxj@antgroup.com>
2025-09-04 18:00:07 +0800

    fix relocation type of call vm
    Signed-off-by: hexuejin <hexuejin.hxj@antgroup.com>

--- test/hotspot/jtreg/compiler/jeandle/relocation/TestRuntimeCallRelocation.java
+++ test/hotspot/jtreg/compiler/jeandle/relocation/TestRuntimeCallRelocation.java
@@ -25,36 +25,40 @@
  * @build jdk.test.whitebox.WhiteBox
  * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
- *      -XX:CompileCommand=compileonly,TestSafepointPoll::test -Xcomp -XX:-TieredCompilation
- *      -XX:+UseJeandleCompiler -Xlog:thread*=trace::time,tags -XX:+JeandleDumpIR -XX:+JeandleDumpObjects -XX:+JeandleDumpRuntimeStubs TestSafepointPoll
+ *      -XX:CompileCommand=compileonly,compiler.jeandle.relocation.TestRuntimeCallRelocation::test -Xcomp -XX:-TieredCompilation
+ *      -XX:+UseJeandleCompiler -Xlog:thread*=trace::time,tags -XX:+JeandleDumpIR -XX:+JeandleDumpObjects -XX:+JeandleDumpRuntimeStubs
+ *      compiler.jeandle.relocation.TestRuntimeCallRelocation
  */
+
+package compiler.jeandle.relocation;
 
 import java.lang.reflect.Method;
 
 import jdk.test.whitebox.WhiteBox;
 
-public class TestSafepointPoll {
+public class TestRuntimeCallRelocation {
     private final static WhiteBox wb = WhiteBox.getWhiteBox();
     static volatile boolean stop = false;
+    static int count = 0;
 
     public static void main(String[] args) throws Exception {
         new Thread(() -> {
             test();
         }).start();
 
-        Method method = TestSafepointPoll.class.getDeclaredMethod("test");
+        Method method = TestRuntimeCallRelocation.class.getDeclaredMethod("test");
         while (!wb.isMethodCompiled(method)) {
             Thread.yield();
         }
 
         Thread.sleep(100);
 
-        wb.forceSafepoint();
-
         stop = true;
     }
 
     static void test() {
-        while (!stop) {}
+        while (!stop) {
+            wb.fullGC();
+        }
     }
 }
