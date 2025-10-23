be5a09ce7ac2c8852a768c513002123a5aca42ca
hexuejin <hexuejin.hxj@antgroup.com>
2025-10-23 19:52:18 +0800

    Check for already marked card before updating it
    CO_REPO: https://github.com/Cruise20/jeandle-llvm.git
    CO_BRANCH: cardtable_barrier
    
    Signed-off-by: hexuejin <hexuejin.hxj@antgroup.com>

--- test/hotspot/jtreg/compiler/jeandle/TestCardTableBarrier.java
+++ test/hotspot/jtreg/compiler/jeandle/TestCardTableBarrier.java
@@ -20,17 +20,21 @@
 
 /**
  * @test
- * @library /test/lib
- * @build jdk.test.whitebox.WhiteBox
+ * @library /test/lib /
+ * @build jdk.test.whitebox.WhiteBox compiler.jeandle.fileCheck.FileCheck
  * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
- *      -XX:+UseSerialGC -XX:MaxTenuringThreshold=2
- *      -XX:CompileCommand=compileonly,TestCardTableBarrier::test*
- *      -Xcomp -XX:-TieredCompilation -XX:+UseJeandleCompiler TestCardTableBarrier
+ *      -XX:+UseSerialGC -XX:MaxTenuringThreshold=2 -XX:CompileCommand=compileonly,TestCardTableBarrier::test*
+ *      -Xcomp -XX:-TieredCompilation -XX:+UseJeandleCompiler -XX:+JeandleDumpIR TestCardTableBarrier
+ * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+ *      -XX:+UseSerialGC -XX:MaxTenuringThreshold=2 -XX:CompileCommand=compileonly,TestCardTableBarrier::test*
+ *      -Xcomp -XX:-TieredCompilation -XX:+UseJeandleCompiler -XX:+JeandleDumpIR -XX:+UseCondCardMark TestCardTableBarrier
  */
 
 import jdk.test.lib.Asserts;
 import jdk.test.whitebox.WhiteBox;
+
+import compiler.jeandle.fileCheck.FileCheck;
 
 public class TestCardTableBarrier {
     private final static WhiteBox wb = WhiteBox.getWhiteBox();
@@ -81,8 +85,8 @@ public class TestCardTableBarrier {
         OldObject oldObject = new OldObject(0);
         Class.forName("TestCardTableBarrier$YoungGenObject");
 
-        // Trigger young GCs to promote the oldObject to the old generation.
-        // Number of young should be greater than MaxTenuringThreshold.
+        // Trigger young GC to promote the oldObject to the old generation.
+        // Number of young GC should be greater than MaxTenuringThreshold.
         wb.youngGC();
         wb.youngGC();
         wb.youngGC();
@@ -91,5 +95,34 @@ public class TestCardTableBarrier {
 
         testYGC(oldObject);
         testFGC(oldObject);
+
+        String currentDir = System.getProperty("user.dir");
+        {
+            if (wb.getVMFlag("UseCondCardMark").toString().equals("false")) {
+                FileCheck fileCheck = new FileCheck(currentDir, TestCardTableBarrier.class.getDeclaredMethod("testYGC", OldObject.class), false, 0);
+                fileCheck.check("define private hotspotcc void @jeandle.card_table_barrier(ptr addrspace(1) %addr)");
+                fileCheck.checkNext("entry:");
+                fileCheck.checkNext("%0 = ptrtoint ptr addrspace(1) %addr to i64");
+                fileCheck.checkNext("%1 = lshr i64 %0, 9");
+                fileCheck.checkNext("%2 = getelementptr inbounds i8, ptr inttoptr");
+                fileCheck.checkNext("store i8 0, ptr %2, align 1");
+                fileCheck.checkNext("ret void");
+            } else {
+                FileCheck fileCheck = new FileCheck(currentDir, TestCardTableBarrier.class.getDeclaredMethod("testYGC", OldObject.class), false, 1);
+                fileCheck.check("define private hotspotcc void @jeandle.card_table_barrier(ptr addrspace(1) %addr)");
+                fileCheck.checkNext("entry:");
+                fileCheck.checkNext("%0 = ptrtoint ptr addrspace(1) %addr to i64");
+                fileCheck.checkNext("%1 = lshr i64 %0, 9");
+                fileCheck.checkNext("%2 = getelementptr inbounds i8, ptr inttoptr");
+                fileCheck.checkNext("%3 = load i8, ptr %2, align 1");
+                fileCheck.checkNext("%4 = icmp eq i8 %3, 0");
+                fileCheck.checkNext("br i1 %4, label %already_dirty, label %store_dirty");
+                fileCheck.checkNext("already_dirty:");
+                fileCheck.checkNext("ret void");
+                fileCheck.checkNext("store_dirty:");
+                fileCheck.checkNext("store i8 0, ptr %2, align 1");
+                fileCheck.checkNext("br label %already_dirty");
+            }
+        }
     }
 }
