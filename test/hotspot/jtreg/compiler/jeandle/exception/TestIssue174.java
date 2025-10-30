d27804a6370e0aeae15102c053d1346808b42e49
taofengliu <liutaofeng.ltf@antgroup.com>
2025-10-30 02:39:45 +0000

    add test
    CO_REPO:https://github.com/taofengliu/jeandle-llvm.git
    CO_BRANCH:fix_landingpad
    
    Signed-off-by: taofengliu <liutaofeng.ltf@antgroup.com>

---
+++ test/hotspot/jtreg/compiler/jeandle/exception/TestIssue174.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2025, the Jeandle-JDK Authors. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+/**
+ * @test
+ * @summary https://github.com/jeandle/jeandle-jdk/issues/174
+ * @requires os.arch=="amd64" | os.arch=="x86_64"
+ * @library /test/lib /
+ * @run main/othervm -XX:CompileCommand=compileonly,compiler.jeandle.exception.TestIssue174::addCounter
+ *      -Xcomp -XX:-TieredCompilation -XX:+UseJeandleCompiler compiler.jeandle.exception.TestIssue174
+ */
+
+package compiler.jeandle.exception;
+
+public class TestIssue174 {
+    static int x;
+
+    public static void main(String[] args) throws Exception {
+        Class.forName("java.lang.System");
+        addCounter(0);
+    }
+
+    static void addOne(int threadID) {
+        x++;
+    }
+
+    static void addCounter(int ID) {
+        for (int j = 0; j < 100000; j++) {
+            if (ID >= 3 || ID < 0) {
+                System.out.println(ID);
+            }
+            addOne(ID);
+        }
+    }
+}
