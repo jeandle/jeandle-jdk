0aae1e5cc1ad6bdfd3f0fbe044887966ef41f271
hexuejin <hexuejin.hxj@antgroup.com>
2025-12-04 17:47:02 +0800

    Update tests
    Signed-off-by: hexuejin <hexuejin.hxj@antgroup.com>

--- test/hotspot/jtreg/compiler/jeandle/intrinsic/TestCosDouble.java
+++ test/hotspot/jtreg/compiler/jeandle/intrinsic/TestCosDouble.java
@@ -48,12 +48,12 @@ public class TestCosDouble {
             "-Xbatch", "-XX:-TieredCompilation", "-XX:+UseJeandleCompiler", "-Xcomp",
             "-Xlog:jeandle=debug", "-XX:+JeandleDumpIR",
             "-XX:JeandleDumpDirectory="+dump_path,
-            "-XX:CompileCommand=compileonly,"+TestWrapper.class.getName()+"::cos_double",
+            "-XX:CompileCommand=compileonly,"+TestEqualsWrapper.class.getName()+"::cos_double",
             "-XX:+JeandleUseRuntimeIntrinsics"));
         if (is_x86) {
           command_args.addAll(List.of("-XX:+UnlockDiagnosticVMOptions", "-XX:+UseLibmIntrinsic"));
         }
-        command_args.add(TestWrapper.class.getName());
+        command_args.add(TestEqualsWrapper.class.getName());
 
         ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(command_args);
         OutputAnalyzer output = ProcessTools.executeCommand(pb);
@@ -62,9 +62,9 @@ public class TestCosDouble {
               .shouldContain("Method `static jdouble java.lang.Math.cos(jdouble)` is parsed as intrinsic");
 
         // Verify llvm IR
-        FileCheck checker = new FileCheck(dump_path, TestWrapper.class.getMethod("cos_double", double.class), false);
+        FileCheck checker = new FileCheck(dump_path, TestEqualsWrapper.class.getMethod("cos_double", double.class), false);
         // find compiled method
-        checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestCosDouble$TestWrapper_cos_double");
+        checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestCosDouble$TestEqualsWrapper_cos_double");
         // check IR
         checker.checkNext("entry:");
         checker.checkNext("br label %bci_0");
@@ -84,17 +84,17 @@ public class TestCosDouble {
                 "-Xbatch", "-XX:-TieredCompilation", "-XX:+UseJeandleCompiler", "-Xcomp",
                 "-Xlog:jeandle=debug", "-XX:+JeandleDumpIR",
                 "-XX:JeandleDumpDirectory="+dump_path,
-                "-XX:CompileCommand=compileonly,"+TestWrapper.class.getName()+"::cos_double",
+                "-XX:CompileCommand=compileonly,"+TestEqualsWrapper.class.getName()+"::cos_double",
                 "-XX:+UnlockDiagnosticVMOptions", "-XX:-UseLibmIntrinsic", "-XX:+JeandleUseRuntimeIntrinsics",
-                TestWrapper.class.getName()));
+                TestEqualsWrapper.class.getName()));
             pb = ProcessTools.createLimitedTestJavaProcessBuilder(command_args);
             output = ProcessTools.executeCommand(pb);
             output.shouldHaveExitValue(0)
                 .shouldContain("Method `static jdouble java.lang.Math.cos(jdouble)` is parsed as intrinsic");
             // Verify llvm IR
-            checker = new FileCheck(dump_path, TestWrapper.class.getMethod("cos_double", double.class), false);
+            checker = new FileCheck(dump_path, TestEqualsWrapper.class.getMethod("cos_double", double.class), false);
             // find compiled method
-            checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestCosDouble$TestWrapper_cos_double");
+            checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestCosDouble$TestEqualsWrapper_cos_double");
             // check IR
             checker.checkNext("entry:");
             checker.checkNext("br label %bci_0");
@@ -102,9 +102,40 @@ public class TestCosDouble {
             checker.checkNext("call double @SharedRuntime_dcos");
             checker.checkNext("ret double");
         }
+
+        // intrinsic by LLVM
+        if (is_x86) { // TODO: add support for other architectures
+            dump_path = System.getProperty("java.io.tmpdir")+"/test3";
+            Path tmp3 = Path.of(dump_path);
+            if (!Files.exists(tmp3)) {
+                Files.createDirectory(tmp3);
+            }
+
+            command_args = new ArrayList<String>(List.of(
+                "-Xbatch", "-XX:-TieredCompilation", "-XX:+UseJeandleCompiler", "-Xcomp",
+                "-Xlog:jeandle=debug", "-XX:+JeandleDumpIR",
+                "-XX:JeandleDumpDirectory="+dump_path,
+                "-XX:CompileCommand=compileonly,"+TestApproximateWrapper.class.getName()+"::cos_double",
+                "-XX:-JeandleUseRuntimeIntrinsics",
+                TestApproximateWrapper.class.getName()));
+            pb = ProcessTools.createLimitedTestJavaProcessBuilder(command_args);
+            output = ProcessTools.executeCommand(pb);
+            output.shouldHaveExitValue(0)
+                .shouldContain("Method `static jdouble java.lang.Math.cos(jdouble)` is parsed as intrinsic");
+            // Verify llvm IR
+            checker = new FileCheck(dump_path, TestApproximateWrapper.class.getMethod("cos_double", double.class), false);
+            // find compiled method
+            checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestCosDouble$TestApproximateWrapper_cos_double");
+            // check IR
+            checker.checkNext("entry:");
+            checker.checkNext("br label %bci_0");
+            checker.checkNext("bci_0:");
+            checker.checkNext("call double @llvm.cos.f64");
+            checker.checkNext("ret double");
+        }
     }
 
-    static public class TestWrapper {
+    static public class TestEqualsWrapper {
         static double v = Math.abs(1.0d);   // Force load java.lang.Math class
         public static void main(String[] args) {
             Random random = new Random();
@@ -116,6 +147,31 @@ public class TestCosDouble {
             for (int i=0; i< 1000; i++) {
                 double d = random.nextDouble();
                 Asserts.assertEquals(cos_double_verified(d) , cos_double(d));
+            }
+        }
+
+        public static double cos_double(double a) {
+            return Math.cos(a);
+        }
+
+        public static double cos_double_verified(double a) {
+            return Math.cos(a);
+        }
+    }
+
+    static public class TestApproximateWrapper {
+        static double v = Math.abs(1.0d);   // Force load java.lang.Math class
+        static double epsilon = 1e-15;
+        public static void main(String[] args) {
+            Random random = new Random();
+            Asserts.assertLessThan(Math.abs(cos_double_verified(1.5d) - cos_double(1.5d)), epsilon);
+            Asserts.assertLessThan(Math.abs(cos_double_verified(-1.5d) - cos_double(-1.5d)), epsilon);
+            Asserts.assertEquals(cos_double_verified(Double.NaN), cos_double(Double.NaN));
+            Asserts.assertEquals(cos_double_verified(Double.POSITIVE_INFINITY), cos_double(Double.POSITIVE_INFINITY));
+            Asserts.assertEquals(cos_double_verified(Double.NEGATIVE_INFINITY), cos_double(Double.NEGATIVE_INFINITY));
+            for (int i=0; i< 1000; i++) {
+                double d = random.nextDouble();
+                Asserts.assertLessThan(Math.abs(cos_double_verified(d) - cos_double(d)), epsilon);
             }
         }
 
--- test/hotspot/jtreg/compiler/jeandle/intrinsic/TestTanDouble.java
+++ test/hotspot/jtreg/compiler/jeandle/intrinsic/TestTanDouble.java
@@ -49,9 +49,9 @@ public class TestTanDouble {
                 "-Xbatch", "-XX:-TieredCompilation", "-XX:+UseJeandleCompiler", "-Xcomp",
                 "-Xlog:jeandle=debug", "-XX:+JeandleDumpIR",
                 "-XX:JeandleDumpDirectory="+dump_path,
-                "-XX:CompileCommand=compileonly,"+TestWrapper.class.getName()+"::tan_double",
+                "-XX:CompileCommand=compileonly,"+TestEqualsWrapper.class.getName()+"::tan_double",
                 "-XX:+UnlockDiagnosticVMOptions", "-XX:+UseLibmIntrinsic", "-XX:+JeandleUseRuntimeIntrinsics",
-                TestWrapper.class.getName()));
+                TestEqualsWrapper.class.getName()));
 
             ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(command_args);
             OutputAnalyzer output = ProcessTools.executeCommand(pb);
@@ -60,9 +60,9 @@ public class TestTanDouble {
                 .shouldContain("Method `static jdouble java.lang.Math.tan(jdouble)` is parsed as intrinsic");
 
             // Verify llvm IR
-            FileCheck checker = new FileCheck(dump_path, TestWrapper.class.getMethod("tan_double", double.class), false);
+            FileCheck checker = new FileCheck(dump_path, TestEqualsWrapper.class.getMethod("tan_double", double.class), false);
             // find compiled method
-            checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestTanDouble$TestWrapper_tan_double");
+            checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestTanDouble$TestEqualsWrapper_tan_double");
             // check IR
             checker.checkNext("entry:");
             checker.checkNext("br label %bci_0");
@@ -82,29 +82,60 @@ public class TestTanDouble {
             "-Xbatch", "-XX:-TieredCompilation", "-XX:+UseJeandleCompiler", "-Xcomp",
             "-Xlog:jeandle=debug", "-XX:+JeandleDumpIR",
             "-XX:JeandleDumpDirectory="+dump_path,
-            "-XX:CompileCommand=compileonly,"+TestWrapper.class.getName()+"::tan_double",
+            "-XX:CompileCommand=compileonly,"+TestEqualsWrapper.class.getName()+"::tan_double",
             "-XX:+JeandleUseRuntimeIntrinsics"));
         if (is_x86) {
             command_args.addAll(List.of("-XX:+UnlockDiagnosticVMOptions", "-XX:-UseLibmIntrinsic"));
         }
-        command_args.add(TestWrapper.class.getName());
+        command_args.add(TestEqualsWrapper.class.getName());
         ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(command_args);
         OutputAnalyzer output = ProcessTools.executeCommand(pb);
         output.shouldHaveExitValue(0)
             .shouldContain("Method `static jdouble java.lang.Math.tan(jdouble)` is parsed as intrinsic");
         // Verify llvm IR
-        FileCheck checker = new FileCheck(dump_path, TestWrapper.class.getMethod("tan_double", double.class), false);
+        FileCheck checker = new FileCheck(dump_path, TestEqualsWrapper.class.getMethod("tan_double", double.class), false);
         // find compiled method
-        checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestTanDouble$TestWrapper_tan_double");
+        checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestTanDouble$TestEqualsWrapper_tan_double");
         // check IR
         checker.checkNext("entry:");
         checker.checkNext("br label %bci_0");
         checker.checkNext("bci_0:");
         checker.checkNext("call double @SharedRuntime_dtan");
         checker.checkNext("ret double");
+
+        // intrinsic by LLVM
+        if (is_x86) {  // TODO: Add support for other architectures.
+            dump_path = System.getProperty("java.io.tmpdir")+"/test3";
+            Path tmp3 = Path.of(dump_path);
+            if (!Files.exists(tmp3)) {
+                Files.createDirectory(tmp3);
+            }
+
+            command_args = new ArrayList<String>(List.of(
+                "-Xbatch", "-XX:-TieredCompilation", "-XX:+UseJeandleCompiler", "-Xcomp",
+                "-Xlog:jeandle=debug", "-XX:+JeandleDumpIR",
+                "-XX:JeandleDumpDirectory="+dump_path,
+                "-XX:CompileCommand=compileonly,"+TestApproximateWrapper.class.getName()+"::tan_double",
+                "-XX:-JeandleUseRuntimeIntrinsics"));
+            command_args.add(TestApproximateWrapper.class.getName());
+            pb = ProcessTools.createLimitedTestJavaProcessBuilder(command_args);
+            output = ProcessTools.executeCommand(pb);
+            output.shouldHaveExitValue(0)
+                .shouldContain("Method `static jdouble java.lang.Math.tan(jdouble)` is parsed as intrinsic");
+            // Verify llvm IR
+            checker = new FileCheck(dump_path, TestApproximateWrapper.class.getMethod("tan_double", double.class), false);
+            // find compiled method
+            checker.check("define hotspotcc double @\"compiler_jeandle_intrinsic_TestTanDouble$TestApproximateWrapper_tan_double");
+            // check IR
+            checker.checkNext("entry:");
+            checker.checkNext("br label %bci_0");
+            checker.checkNext("bci_0:");
+            checker.checkNext("call double @llvm.tan.f64");
+            checker.checkNext("ret double");
+        }
     }
 
-    static public class TestWrapper {
+    static public class TestEqualsWrapper {
         static double v = Math.abs(1.0d);   // Force load java.lang.Math class
         public static void main(String[] args) {
             Random random = new Random();
@@ -116,6 +147,31 @@ public class TestTanDouble {
             for (int i=0; i< 1000; i++) {
                 double d = random.nextDouble();
                 Asserts.assertEquals(tan_double_verified(d) , tan_double(d));
+            }
+        }
+
+        public static double tan_double(double a) {
+            return Math.tan(a);
+        }
+
+        public static double tan_double_verified(double a) {
+            return Math.tan(a);
+        }
+    }
+
+    static public class TestApproximateWrapper {
+        static double v = Math.abs(1.0d);   // Force load java.lang.Math class
+        static double epsilon = 1e-15;
+        public static void main(String[] args) {
+            Random random = new Random();
+            Asserts.assertLessThan(Math.abs(tan_double_verified(1.5d) - tan_double(1.5d)), epsilon);
+            Asserts.assertLessThan(Math.abs(tan_double_verified(-1.5d) - tan_double(-1.5d)), epsilon);
+            Asserts.assertEquals(tan_double_verified(Double.NaN), tan_double(Double.NaN));
+            Asserts.assertEquals(tan_double_verified(Double.POSITIVE_INFINITY), tan_double(Double.POSITIVE_INFINITY));
+            Asserts.assertEquals(tan_double_verified(Double.NEGATIVE_INFINITY), tan_double(Double.NEGATIVE_INFINITY));
+            for (int i=0; i< 1000; i++) {
+                double d = random.nextDouble();
+                Asserts.assertLessThan(Math.abs(tan_double_verified(d) - tan_double(d)), epsilon);
             }
         }
 
