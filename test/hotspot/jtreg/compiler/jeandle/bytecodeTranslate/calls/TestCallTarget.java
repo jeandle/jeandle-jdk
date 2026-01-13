0d5bd5f6a3b458172e89f7e69de837fa54ab7479
taofengliu <liutaofeng.ltf@antgroup.com>
2026-01-13 08:31:44 +0000

    add test
    Signed-off-by: taofengliu <liutaofeng.ltf@antgroup.com>

--- test/hotspot/jtreg/compiler/jeandle/bytecodeTranslate/calls/TestCallTarget.java
+++ test/hotspot/jtreg/compiler/jeandle/bytecodeTranslate/calls/TestCallTarget.java
@@ -20,11 +20,16 @@
 
 package compiler.jeandle.bytecodeTranslate.calls;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+
 /**
  * @test
  * @summary https://github.com/jeandle/jeandle-jdk/issues/290
  * @run main/othervm -Xcomp -XX:-TieredCompilation -Xbatch
  *      -XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.calls.TestCallTarget::getString
+ *      -XX:CompileCommand=compileonly,java.math.BigDecimal::add
  *      -XX:+UseJeandleCompiler compiler.jeandle.bytecodeTranslate.calls.TestCallTarget
  */
 
@@ -36,9 +41,25 @@ public class TestCallTarget {
         return s;
     }
     public static void main(String[] args) throws Exception {
+        // Test code from https://github.com/jeandle/jeandle-jdk/issues/290
         Object o = new String("hello");
         if (!getString(o).equals("hello")) {
             throw new RuntimeException("test failed");
         }
+
+        // Test code from https://github.com/jeandle/jeandle-jdk/issues/285
+        // Load classes first
+        MathContext mc = new MathContext(10);
+        BigDecimal b = new BigDecimal(new BigInteger("7812404666936930160"), 11);
+        BigDecimal b2 = new BigDecimal(new BigInteger("7812404666936930160"), 11);
+        b.add(b2, mc);
+        test();
+    }
+
+    static void test() {
+        MathContext mc = new MathContext(10);
+        BigDecimal b = new BigDecimal(new BigInteger("7812404666936930160"), 11);
+        BigDecimal b2 = new BigDecimal(new BigInteger("7812404666936930160"), 11);
+        b.add(b2, mc);
     }
 }
