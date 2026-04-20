cb608c64a2f6c35832d529269700a7c284f092cb
hexuejin <hexuejin.hxj@antgroup.com>
2026-04-20 11:57:46 +0000

    fix cases
    Signed-off-by: hexuejin <hexuejin.hxj@antgroup.com>

--- test/hotspot/jtreg/compiler/jeandle/bytecodeTranslate/alloc/TestAllocFastSlowPath.java
+++ test/hotspot/jtreg/compiler/jeandle/bytecodeTranslate/alloc/TestAllocFastSlowPath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, the Jeandle-JDK Authors. All Rights Reserved.
+ * Copyright (c) 2025, 2026, the Jeandle-JDK Authors. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,15 +30,46 @@ package compiler.jeandle.bytecodeTranslate.alloc;
 import java.util.ArrayList;
 import java.util.List;
 
+import jdk.test.lib.Asserts;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
 
 public class TestAllocFastSlowPath {
 
+    private int _val = 42;
+
     public static void main(String[] args) throws Exception {
+        if (args.length == 1) {
+            // Child process mode
+            if (args[0].equals("normal")) {
+                Asserts.assertEquals(allocate_java_instance(), 42);
+            } else if (args[0].equals("stress")) {
+                // Pre-load BigClass before Jeandle compiles the method,
+                // to avoid deoptimization.
+                new BigClass();
+                Asserts.assertEquals(stress_allocate_java_instance(100_000), 42L * 100_000);
+            }
+            return;
+        }
+
+        // Driver mode: run sub-tests
         testFastPathSmallObject();
         testSlowPathTLABExhaustion();
         testSlowPathUseTLABDisabled();
+    }
+
+    public static int allocate_java_instance() {
+        TestAllocFastSlowPath obj = new TestAllocFastSlowPath();
+        return obj._val;
+    }
+
+    public static long stress_allocate_java_instance(int loop) {
+        long sum = 0;
+        for (int i = 0; i < loop; i++) {
+            BigClass obj = new BigClass();
+            sum += obj.x;
+        }
+        return sum;
     }
 
     // Small object with sufficient TLAB space should use fast path (no slow path log)
@@ -49,8 +80,9 @@ public class TestAllocFastSlowPath {
             "-XX:-TieredCompilation",
             "-XX:+UseJeandleCompiler",
             "-Xlog:jeandle+alloc=debug",
-            "-XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.alloc.TestNewObject::allocate_java_instance",
-            TestNewObject.class.getName()
+            "-XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.alloc.TestAllocFastSlowPath::allocate_java_instance",
+            TestAllocFastSlowPath.class.getName(),
+            "normal"
         ));
         OutputAnalyzer output = ProcessTools.executeCommand(pb);
         output.shouldHaveExitValue(0);
@@ -67,13 +99,13 @@ public class TestAllocFastSlowPath {
             "-XX:-TieredCompilation",
             "-XX:+UseJeandleCompiler",
             "-Xlog:jeandle+alloc=debug",
-            "-XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.alloc.TestNewObject::*",
-            TestNewObject.class.getName(),
+            "-XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.alloc.TestAllocFastSlowPath::*",
+            TestAllocFastSlowPath.class.getName(),
             "stress"
         ));
         OutputAnalyzer output = ProcessTools.executeCommand(pb);
         output.shouldHaveExitValue(0);
-        output.shouldContain("Slow path allocation for compiler.jeandle.bytecodeTranslate.alloc.TestNewObject$BigClass");
+        output.shouldContain("Slow path allocation for compiler.jeandle.bytecodeTranslate.alloc.TestAllocFastSlowPath$BigClass");
     }
 
     // With -XX:-UseTLAB, all allocations should go through slow path
@@ -85,11 +117,69 @@ public class TestAllocFastSlowPath {
             "-XX:-UseTLAB",
             "-XX:+UseJeandleCompiler",
             "-Xlog:jeandle+alloc=debug",
-            "-XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.alloc.TestNewObject::allocate_java_instance",
-            TestNewObject.class.getName()
+            "-XX:CompileCommand=compileonly,compiler.jeandle.bytecodeTranslate.alloc.TestAllocFastSlowPath::allocate_java_instance",
+            TestAllocFastSlowPath.class.getName(),
+            "normal"
         ));
         OutputAnalyzer output = ProcessTools.executeCommand(pb);
         output.shouldHaveExitValue(0);
-        output.shouldContain("Slow path allocation for compiler.jeandle.bytecodeTranslate.alloc.TestNewObject");
+        output.shouldContain("Slow path allocation for compiler.jeandle.bytecodeTranslate.alloc.TestAllocFastSlowPath");
+    }
+
+    // A big class to make TLAB exhaustion likely under small heap
+    static class BigClass {
+        long a;
+        long b;
+        long c;
+        long d;
+        long e;
+        long f;
+        long g;
+        long h;
+        long i;
+        long j;
+        long k;
+        long l;
+        long m;
+        long n;
+        long o;
+        long p;
+        long q;
+        long r;
+        long s;
+        long t;
+        long u;
+        long v;
+        long w;
+        long x = 42L;
+        long y;
+        long z;
+
+        double d_a;
+        double d_b;
+        double d_c;
+        double d_d;
+        double d_e;
+        double d_f;
+        double d_g;
+        double d_h;
+        double d_i;
+        double d_j;
+        double d_k;
+        double d_l;
+        double d_m;
+        double d_n;
+        double d_o;
+        double d_p;
+        double d_q;
+        double d_r;
+        double d_s;
+        double d_t;
+        double d_u;
+        double d_v;
+        double d_w;
+        double d_x;
+        double d_y;
+        double d_z;
     }
 }
