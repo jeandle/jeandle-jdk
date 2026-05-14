40acbb77f661f16688c7512275757c8face0f5e0
WenjieZhang <78072662+yirenchuanmu@users.noreply.github.com>
2026-05-14 10:09:39 +0800

    Delete test.sh
    Signed-off-by: WenjieZhang <78072662+yirenchuanmu@users.noreply.github.com>

--- test.sh
+++
@@ -1,6 +0,0 @@
-jtreg \
-  -jdk:./build/linux-x86_64-server-fastdebug/images/jdk/ \
-  -vmoption:-XX:+UseJeandleCompiler \
-  -verbose:fail,error,time \
-  -timeoutFactor:20 \
-  test/hotspot/jtreg/compiler/c2/TestShiftRightAndAccumulate.java
