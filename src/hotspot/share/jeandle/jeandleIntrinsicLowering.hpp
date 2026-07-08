490f07793dfe1c8ec73ed86b34a2cca97c0c10f2
hexuejin <hexuejin.hxj@antgroup.com>
2026-07-08 08:21:54 +0000

    Merge hashCode/identityHashCode lowering and replace slow path with java call
    Signed-off-by: hexuejin <hexuejin.hxj@antgroup.com>
    Signed-off-by: eapen <zhangyipeng7@huawei.com>

--- src/hotspot/share/jeandle/jeandleIntrinsicLowering.hpp
+++ src/hotspot/share/jeandle/jeandleIntrinsicLowering.hpp
@@ -207,8 +207,7 @@ class JeandleIntrinsicLowering : public StackObj {
                                                llvm::Value* b_addr,
                                                llvm::Value* byte_length,
                                                llvm::Value* scale);
-  bool lower_identity_hash_code();
-  bool lower_hash_code();
+  bool lower_hash_code(vmIntrinsics::ID id);
 
   };
 
